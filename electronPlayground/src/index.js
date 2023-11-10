cdc72a299b854c4ace960bd7c0ed99e52968a529
Filotas Sismanis <platwnas011@gmail.com>
2023-11-10 16:03:50 +0200

    Changes to package.json and autoUpdater

--- electronPlayground/src/index.js
+++ electronPlayground/src/index.js
@@ -1,10 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, autoUpdater } = require('electron');
 const path = require('path');
+// const { autoUpdater, AppUpdater } = require('electron-updater')
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
+
+// autoUpdater.autoDownload = false
+// autoUpdater.autoInstallOnAppQuit = true
+autoUpdater.setFeedURL('https://github.com/philSism/electronPlayground')
 
 const createWindow = () => {
   // Create the browser window.
@@ -26,7 +31,11 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow()
+
+  autoUpdater.checkForUpdates()
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -44,6 +53,25 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+autoUpdater.on('update-available', () => {
+  console.info('update ready')
+  document.getElementById('updateMessage').innerText = 'Update available'
+})
+
+autoUpdater.on('update-not-available', () => {
+  console.info('no update')
+  document.getElementById('updateMessage').innerText = 'App is fully updated'
+})
+
+autoUpdater.on('update-downloaded', () => {
+  document.getElementById('updateMessage').innerText = 'Update download: completed'
+})
+
+autoUpdater.on('error', () => {
+  console.info('error')
+  document.getElementById('updateMessage').innerText = 'AutoUpdater: ERROR'
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
